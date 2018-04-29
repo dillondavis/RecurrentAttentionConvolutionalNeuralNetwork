098b053f594e655887844cbadeb4b10e21334faf
dadavis21 <dadavis21@yahoo.com>
2018-04-29 17:19:56 -0500

    move eval after train

--- src/networks.py
+++ src/networks.py
@@ -105,28 +105,25 @@ class APN(nn.Module):
 class NegSquareNormGradients(torch.autograd.Function):
 
     # Note that both forward and backward are @staticmethods
-    @staticmethod
     # bias is an optional argument
-    def forward(ctx, input, weight, bias=None):
-        ctx.save_for_backward(input)
+    def forward(self, input):
+        self.save_for_backward(input)
         return input
 
     # This function has only a single output, so it gets only one gradient
-    @staticmethod
-    def backward(ctx, grad_output):
+    def backward(self, grad_output):
         # This is a pattern that is very convenient - at the top of backward
         # unpack saved_tensors and initialize all gradients w.r.t. inputs to
         # None. Thanks to the fact that additional trailing Nones are
         # ignored, the return statement is simple even when the function has
         # optional inputs.
-        input = ctx.saved_tensors
-        grad_input = None
 
         # These needs_input_grad checks are optional and there only to
         # improve efficiency. If you want to make your code simpler, you can
         # skip them. Returning gradients for inputs that don't require it is
         # not an error.
-        if ctx.needs_input_grad[0]:
+        grad_input = None
+        if self.needs_input_grad[0]:
             grad_input = -(grad_output.norm()**2)
 
         return grad_input
